f7d3e793853ae127b3a7f38fc7591f90372bd2e3
Robin Schürer <r.schuerer@braincode.de>
2017-06-06 14:47:02 +0200

    test file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Read enron.json and upload to elastic search
+* Command line tool
+* JSON Streaming
+* Read json datasets from stream and upload this dataset to elastic search
+
+## Usage
+java -jar build.jar -input src/main/resources/enjon.json
--- src/main/java/de/robinschuerer/base/indexservice/ElasticSearchIndexService.java
+++ src/main/java/de/robinschuerer/base/indexservice/ElasticSearchIndexService.java
@@ -1,15 +1,71 @@
 package de.robinschuerer.base.indexservice;
 
 import java.io.File;
+import java.io.IOException;
 
+import javax.annotation.Nonnull;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Service
 public class ElasticSearchIndexService {
 
-    private final static String INDEX_NAME = "data";
+    private final static String INDEX_NAME = "enron";
 
-    public void index(final File dataFile) {
+    private final static String URL = "http://localhost:9200/" + INDEX_NAME;
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public void index(@Nonnull final File dataFile) throws IOException {
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final JsonFactory factory = new JsonFactory(objectMapper);
+        final JsonParser parser = factory.createParser(dataFile);
+
+        while (true) {
+            final JsonToken token = parser.nextToken();
+            if (token == null) {
+                break;
+            }
+
+            final TreeNode tree = parser.readValueAsTree();
+
+//            final TextNode idNode = (TextNode) tree.get("_id").get("$oid");
+
+            final ObjectNode normalized = (ObjectNode) tree;
+//            normalized.remove("_id");
+//            normalized.set("backup_id", idNode);
+
+//            normalized.remove("ctype");
+//            normalized.remove("date");
+
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            final HttpEntity<String> request = new HttpEntity<>(normalized.toString(), headers);
+
+            System.out.println(request.getBody());
+
+            try {
+                restTemplate.put(URL, request);
+            } catch (HttpClientErrorException e) {
+                System.out.println(e.getResponseBodyAsString());
+
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 }
